19b87f10eabf02cd6bc0f37f2ec4e86909eff5d0
Emrul <emrulhossain49@gmail.com>
2024-07-03 15:52:10 +0600

    Add files via upload

---
+++ Problem-1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printArray(int arr[], int size) {
+    cout << "Array in forward order:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "Array in reverse order:" << endl;
+    for (int i = 0; i < size; ++i) {
+        cout << arr[size - 1 - i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    printArray(arr, size);
+    return 0;
+}
