8463c47267d840eafbd0b40988c0ba22f7b1d009
Emrul <emrulhossain49@gmail.com>
2024-10-04 00:43:14 +0600

    Create pop_window.cpp

---
+++ Fun/pop_window.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 100; ++i) {
+        system("start cmd");  // Opens a new console window (on Windows)
+    }
+    return 0;
+}
