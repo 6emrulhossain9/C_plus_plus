9a4e0e4516c7311577c3ad7b8577edbdf7f2b674
Emrul <emrulhossain49@gmail.com>
2024-10-04 00:48:46 +0600

    Update v_p.cpp

--- Fun/v_p.cpp
+++ Fun/v_p.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+int main() {
+    cout << "WARNING: Virus detected! System will be wiped in 10 seconds...\n";
+    for (int i = 10; i > 0; i--) {
+        cout << "Time left: " << i << " seconds...\n";
+        this_thread::sleep_for(chrono::seconds(1));  // Wait for 1 second
+    }
+    cout << "Deleting all files...\n";
+    this_thread::sleep_for(chrono::seconds(3));
+    cout << "ERROR: Access denied! Virus failed to delete files.\n";
+    cout << "You're safe! Just kidding, it's a prank :)\n";
+    return 0;
+}
 
